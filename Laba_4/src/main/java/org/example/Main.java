f7c131e1d4671e898b27dc6de17fe7cac5b22a34
Vladonus <116440317+Vladonus@users.noreply.github.com>
2022-12-07 09:26:09 +0300

    Update Main.java

--- Laba_4/src/main/java/org/example/Main.java
+++ Laba_4/src/main/java/org/example/Main.java
@@ -7,12 +7,15 @@ import au.com.bytecode.opencsv.CSVReader;
 public class Main {
     public static void main(String[] args)throws Exception {
         ArrayList<Person> Spisok = new ArrayList<Person>();
-        CSVReader input = new CSVReader(new FileReader("E:\\Загрузки\\foreign_names\\foreign_names.csv"), ';' ,'"' , 1);
-        String[] nextLine = input.readNext();
+        CSVReader excel = new CSVReader(new FileReader("E:\\Загрузки\\foreign_names\\foreign_names.csv"), ';' ,'"' , 1);
+        if(excel==null) {
+            throw new RuntimeException("file read error");
+        }
+        String[] nextLine = excel.readNext();
         while ((nextLine) != null) {
             Person newLine = new Person(nextLine);
             Spisok.add(newLine);
-            nextLine = input.readNext();
+            nextLine = excel.readNext();
         }
 
         for (int i = 0;i < Spisok.size();i++)
